bc860413e14422305a7781fcde8dcd1b71c5571b
KSB <KSB@DESKTOP-4IHG4G1>
2019-12-27 15:53:00 +0900

    수정

--- src/main/java/com/goldenbrothers/everylecture/controller/ViewController.java
+++ src/main/java/com/goldenbrothers/everylecture/controller/ViewController.java
@@ -6,12 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.goldenbrothers.everylecture.HomeController;
-
 @Controller
 public class ViewController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
