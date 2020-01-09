21ebf87d68853f3270d8c2380f3d53eb66687d1d
hlgf1114 <woltaryksb1@gmail.com>
2020-01-10 00:51:53 +0900

    이제 빈 제목으로 작성할 수 없습니다.

--- src/main/webapp/resources/js/board/boardRevise.js
+++ src/main/webapp/resources/js/board/boardRevise.js
@@ -12,28 +12,35 @@ $(document).ready(function() {
 	
 	$submit.on("click", function() {
 		
-		var data = {
-				"boardID" : String($('input[name=boardID]').val()),
-				"boardName" : String($('input[name=bTitle]').val()),
-				"boardText" : String($('textarea[name=bContent]').val())
-		};
-		
-		$.ajax({
-			url : "/everylecture/board/boardUpadte",
-			data : data,
-			type : "POST",
-			async : true,
-			success : function(result) {
-				
-				if(result == "1") {
-					alert("수정하였습니다.");
-					location.href="/everylecture/board/boardView/" + $('input[name=boardID]').val();
+		if(!$('input[name=bTitle]').val() == '') {
+			
+			var data = {
+					"boardID" : String($('input[name=boardID]').val()),
+					"boardName" : String($('input[name=bTitle]').val()),
+					"boardText" : String($('textarea[name=bContent]').val())
+			};
+			
+			$.ajax({
+				url : "/everylecture/board/boardUpadte",
+				data : data,
+				type : "POST",
+				async : true,
+				success : function(result) {
+					
+					if(result == "1") {
+						alert("수정하였습니다.");
+						location.href="/everylecture/board/boardView/" + $('input[name=boardID]').val();
+					}
+				},
+				error : function(result) {
+					alert("ERROR");
 				}
-			},
-			error : function(result) {
-				alert("ERROR");
-			}
-		});
+			});
+		}
+		else {
+			alert("제목은 빌 수 없습니다.");
+		}
+		
 		
 	});
 });
--- src/main/webapp/resources/js/board/boardWrite.js
+++ src/main/webapp/resources/js/board/boardWrite.js
@@ -12,23 +12,29 @@ $(document).ready(function() {
 	
 	$submit.on("click", function() {
 		
-		var data = {
-				"boardName" : String($('input[name=bTitle]').val()),
-				"boardText" : String($('textarea[name=bContent]').val())
-		};
-		
-		$.ajax({
-			url : "/everylecture/board/writeContent/",
-			data : data,
-			type : "POST",
-			async : true,
-			success : function(result) {
-				location.href="/everylecture/board/boardForm";
-			},
-			error : function(result) {
+		if(!$('input[name=bTitle]').val() == '') {			
+			var data = {
+					"boardName" : String($('input[name=bTitle]').val()),
+					"boardText" : String($('textarea[name=bContent]').val())
+			};
+			
+			$.ajax({
+				url : "/everylecture/board/writeContent/",
+				data : data,
+				type : "POST",
+				async : true,
+				success : function(result) {
+					location.href="/everylecture/board/boardForm";
+				},
+				error : function(result) {
 //				alert(result);
-			}
-		});
+				}
+			});
+		}
+		else {
+			alert("제목은 빌 수 없습니다.");
+		}
+		
 		
 	});
 });
